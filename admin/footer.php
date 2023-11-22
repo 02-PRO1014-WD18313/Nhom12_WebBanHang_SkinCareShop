ff67fc2ffd723c7ceb5f8a69f1819072e04e6185
uyen2205 <150259619+uyen2205@users.noreply.github.com>
2023-11-22 22:32:25 +0700

    .

--- admin/footer.php
+++ admin/footer.php
@@ -3,7 +3,7 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-md-6">
-                    2023 &copy; Chào mừng đến MenStrore !!!
+                    2023 &copy; Chào mừng đến Skincare shop !!!
                 </div>
                 <div class="col-md-6">
                     <div class="text-md-right footer-links d-none d-sm-block">
